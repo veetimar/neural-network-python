94ea31fc005d6827df25efaf0bb686139af5d36f
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-05-22 17:42:37 +0300

    Add forward propagation

--- src/network.py
+++ src/network.py
@@ -1,4 +1,5 @@
-"""network.py
+"""Network
+==========
 
 This module contains the Network class that can be used to create a neural network.
 """
@@ -14,19 +15,27 @@ class Network:
     training data and should be able to generalize it to cases not seen before.
 
     Attributes:
-        size (tuple): AContains integers whose length is the amount of layers in the network
-          and the elements are the amounts of neurons in each layer respectively.
-          This is also the only parameter for the constructor.
-        values (list): Contains values of every neuron in the network.
-        weights (list): Contains weights of every neuron in the network.
-        biases (list): Contains biases of every neuron in the network.
+        size (tuple): Information regarding the amount of layers and neurons in the network.
+        values (list): Values of every neuron in the network.
+        weights (list): Weights of every neuron in the network.
+        biases (list): Biases of every neuron in the network.
     """
 
-    def __init__(self, *size):
+    def __init__(self, size):
+        """Create a new neural network.
+
+        Args:
+            size (Iterable[int]): The length of this parameter is the amount of layers in
+              the network and the elements are the amounts of neurons in each layer respectively.
+
+        Raises:
+            ValueError: If the amount of layers is under 2 or
+              the amount of neurons in a layer is under 1.
+        """
         if len(size) < 2 or not all(layer_size > 0 for layer_size in size):
             raise ValueError("illegal size for the neural network")
         self.size = tuple(size)
-        self.values = [np.zeros((self.size[i], 1)) for i in range(len(self.size))]
+        self.values = [None for _ in range(len(size))]
         self.weights = [None]
         self.biases = [None]
         for i in range(1, len(self.size)):
@@ -41,12 +50,41 @@ class Network:
             old_size (int): The size of the previous layer.
 
         Returns:
-            NDArray: The weights matrix for the current layer.
+            ndarray: The weights matrix for the current layer.
         """
-
         x = math.sqrt(6 / (size + old_size))
         weights = np.random.uniform(-x, x, (size, old_size))
         return weights
+
+    def sigmoid(self, array):
+        """Apply sigmoid function element-wise to a vector.
+
+        Args:
+            array (ndarray): Array to be worked on.
+
+        Returns:
+            ndarray: Array like the original array but sigmoid applied to the elements.
+        """
+        new_array = np.zeros_like(array)
+        for i, x in enumerate(array):
+            new_array[i] = 1 / (1 + math.exp(-x[0]))
+        return new_array
+
+    def forward(self, inputs):
+        """Forward inputs through the network and return outputs.
+
+        Args:
+            inputs (Iterable[float]): The inputs to be forwarded. Should be floats between 0 and 1.
+              The length of this parameter must be equal to the length of the input layer.
+
+        Returns:
+            ndarray: Values of the output neurons after the forwarding.
+        """
+        self.values[0] = np.reshape(inputs, (self.size[0], 1))
+        for i in range(1, len(self.size)):
+            vector = np.matmul(self.weights[i], self.values[i - 1]) + self.biases[i]
+            self.values[i] = self.sigmoid(vector)
+        return self.values[-1]
 
     def __repr__(self):
         return f"Network{self.size}"
--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -4,24 +4,25 @@ from network import Network
 
 class TestNetwork(unittest.TestCase):
     def setUp(self):
-        self.size = (2, 3, 1)
-        self.network = Network(*self.size)
+        self.size = (3, 4, 2)
+        self.network = Network(list(self.size))
 
     def test_constructor_raises_error(self):
-        self.assertRaises(ValueError, Network, 0, 1)
-        self.assertRaises(ValueError, Network, 1, 0)
-        self.assertRaises(ValueError, Network, 1)
+        self.assertRaises(ValueError, Network, (0, 1))
+        self.assertRaises(ValueError, Network, (1, 0))
+        self.assertRaises(ValueError, Network, (1,))
 
-    def test_constructor_sets_attributes(self):
-        self.assertEqual(self.size, self.network.size)
-        for i, length in enumerate(self.size):
-            self.assertEqual(len(self.network.values[i]), length)
-            if i == 0:
-                self.assertIsNone(self.network.weights[i])
-                self.assertIsNone(self.network.biases[i])
-            else:
-                self.assertEqual(len(self.network.weights[i]), length)
-                self.assertEqual(len(self.network.biases[i]), length)
+    def test_constructor_sets_attributes_shape_right(self):
+        self.assertEqual(len(self.network.values), len(self.size))
+        self.assertEqual(len(self.network.weights), len(self.size))
+        self.assertEqual(len(self.network.biases), len(self.size))
+        for i in range(1, len(self.size)):
+            self.assertEqual(len(self.network.weights[i]), self.size[i])
+            self.assertEqual(len(self.network.weights[i][0]), self.size[i - 1])
+            self.assertEqual(len(self.network.biases[i]), self.size[i])
+
+    def test_forward_returns_right_shape(self):
+        self.assertEqual(len(self.network.forward([0 for _ in range(self.size[0])])), self.size[-1])
 
     def test_repr(self):
         self.assertEqual(repr(self.network), f"Network{self.size}")
