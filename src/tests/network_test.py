edc9c7558bb59f72e24f7c8a5b4fddd700e0a17c
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-05-27 12:18:01 +0300

    Simplify tests

--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -5,7 +5,8 @@ from network import Network
 class TestNetwork(unittest.TestCase):
     def setUp(self):
         self.size = (3, 4, 2)
-        self.network = Network(list(self.size))
+        self.network = Network(self.size)
+        self.zeros = [0 for _ in range(self.size[0])]
 
     def test_constructor_raises_error(self):
         self.assertRaises(ValueError, Network, (0, 1))
@@ -17,12 +18,11 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(len(self.network.weights), len(self.size))
         self.assertEqual(len(self.network.biases), len(self.size))
         for i in range(1, len(self.size)):
-            self.assertEqual(len(self.network.weights[i]), self.size[i])
-            self.assertEqual(len(self.network.weights[i][0]), self.size[i - 1])
-            self.assertEqual(len(self.network.biases[i]), self.size[i])
+            self.assertEqual(self.network.weights[i].shape, (self.size[i], self.size[i - 1]))
+            self.assertEqual(self.network.biases[i].shape, (self.size[i], 1))
 
     def test_forward_returns_right_shape(self):
-        self.assertEqual(len(self.network.forward([0 for _ in range(self.size[0])])), self.size[-1])
+        self.assertEqual(self.network.forward(self.zeros).shape, (self.size[-1], 1))
 
     def test_repr(self):
         self.assertEqual(repr(self.network), f"Network{self.size}")
