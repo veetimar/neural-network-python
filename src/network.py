fbeee270016dac575664e7ff001d85349036d306
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-06-02 17:47:20 +0300

    Add backpropagation

--- src/network.py
+++ src/network.py
@@ -5,6 +5,7 @@ This module contains the Network class that can be used to create a neural netwo
 """
 
 import math
+import random
 
 import numpy as np
 
@@ -15,32 +16,42 @@ class Network:
     training data and should be able to generalize it to cases not seen before.
 
     Attributes:
-        size (tuple): Information regarding the amount of layers and neurons in the network.
+        shape (tuple): Information regarding the amount of layers and neurons in the network.
         values (list): Values of every neuron in the network.
+        activations (list): Values but after applying the activation function.
+        gradients (list): Used during backpropagation to speed up and simplify calculations.
         weights (list): Weights of every neuron in the network.
         biases (list): Biases of every neuron in the network.
+        delta_weights (list): Cached weights used during backpropagation.
+        delta_biases (list): Cached biases used during backpropagation.
     """
 
     def __init__(self, shape):
         """Create a new neural network.
 
         Args:
-            size (Iterable[int]): The length of this parameter is the amount of layers in
+            shape (Iterable[int]): The length of this parameter is the amount of layers in
               the network and the elements are the amounts of neurons in each layer respectively.
 
         Raises:
             ValueError: If the amount of layers is under 2 or
               the amount of neurons in a layer is under 1.
         """
-        if len(shape) < 2 or not all(layer_size > 0 for layer_size in shape):
-            raise ValueError("illegal size for the neural network")
+        if len(shape) < 2 or not all(size > 0 for size in shape):
+            raise ValueError("illegal shape for the neural network")
         self.shape = tuple(shape)
         self.values = [None for _ in range(len(shape))]
+        self.activations = [None for _ in range(len(shape))]
+        self.gradients = [None for _ in range(len(shape))]
         self.weights = [None]
         self.biases = [None]
+        self.delta_weights = [None]
+        self.delta_biases = [None]
         for i in range(1, len(self.shape)):
-            self.biases.append(np.zeros((self.shape[i], 1)))
             self.weights.append(self.xavier(self.shape[i], self.shape[i - 1]))
+            self.biases.append(np.zeros((self.shape[i], 1)))
+            self.delta_weights.append(np.zeros_like(self.weights[i]))
+            self.delta_biases.append(np.zeros_like(self.biases[i]))
 
     def xavier(self, size, old_size):
         """Uniform xavier weight initialization for layers using the sigmoid activation funcion.
@@ -91,7 +102,7 @@ class Network:
             float: The error value.
         """
         x = outputs - expected
-        return np.matmul(x.T, x) / len(x)
+        return float(np.matmul(x.T, x)[0][0] / len(x))
 
     def mean_squared_gradient(self, outputs, expected):
         """Calculate the gradient of mean squared error with respect to outputs.
@@ -116,11 +127,102 @@ class Network:
         Returns:
             ndarray: Values of the output neurons after the forwarding.
         """
-        self.values[0] = np.reshape(inputs, (self.shape[0], 1))
+        self.activations[0] = np.reshape(inputs, (self.shape[0], 1))
         for i in range(1, len(self.shape)):
-            vector = np.matmul(self.weights[i], self.values[i - 1]) + self.biases[i]
-            self.values[i] = self.sigmoid(vector)
-        return self.values[-1]
+            vector = np.matmul(self.weights[i], self.activations[i - 1]) + self.biases[i]
+            self.values[i] = vector
+            self.activations[i] = self.sigmoid(vector)
+        return self.activations[-1]
+
+    def backward(self, inputs, expected):
+        """Backpropagation. 
+
+        Adjust the weights and biases of the network in such a way that the provided inputs will
+        produce outputs more like the expected outputs. Run update afterwards to apply the changes.
+
+        Args:
+            inputs (Iterable[float]): Inputs to the network. Should be floats between 0 and 1.
+              The length of this parameter must be equal to the length of the input layer.
+            expected (Iterable[float]): The expected outputs from the passed inputs.
+              Should be floats between 0 and 1.
+              The length of this parameter must be equal to the length of the output layer.
+
+        Returns:
+            float: Mean squared error calculated from the actual outputs and expected outputs.
+        """
+        inputs = np.reshape(inputs, (self.shape[0], 1))
+        expected = np.reshape(expected, (self.shape[-1], 1))
+        outputs = self.forward(inputs)
+        error = self.mean_squared(outputs, expected)
+        self.gradients[-1] = self.mean_squared_gradient(outputs, expected)
+        self.gradients[-1] *= self.sigmoid_derivative(self.values[-1])
+        for i in range(len(self.shape) - 1, 0, -1):
+            self.delta_biases[i] += self.gradients[i]
+            self.delta_weights[i] += np.matmul(self.gradients[i], self.activations[i - 1].T)
+            if i != 1:
+                self.gradients[i - 1] = np.matmul(self.weights[i].T, self.gradients[i])
+                self.gradients[i - 1] *= self.sigmoid_derivative(self.values[i - 1])
+        return error
+
+    def train(self, training_data, epochs, batch_size=-1, learning_rate=0.01):
+        """Train the neural network.
+
+        Args:
+            training_data (list): Contains tuples containing input and output examples. The first
+              element of the tuple is inputs to the network and the second element is expected
+              outputs.
+            epochs (int): Number of epochs (how many times the whole dataset is iterated through).
+            learning_rate (float, optional): Determines how fast the network tries to learn.
+              The bigger this value the bigger steps the network takes towards the target.
+              Defaults to 0.01.
+            batch_size (int, optional): Determines how many training examples are processed before
+              Updating the weights. Setting this to -1 will set batch size to maximum i.e. the
+              length of the training data. Defaults to -1.
+
+        Raises:
+            ValueError: If the given amount of epochs is not positive.
+            ValueError: If the given batch size is not positive (-1 allowed) or is bigger than the
+              length of the training data.
+            ValueError: If the length of a training example is not 2 (contains something else than
+              inputs and expected outputs).
+
+        Returns:
+            list: Errors of each epoch in order from first to last.
+        """
+        if epochs < 1:
+            raise ValueError("illegal amount of epochs")
+        if batch_size == -1:
+            batch_size = len(training_data)
+        if not 0 < batch_size <= len(training_data):
+            raise ValueError("illegal batch size")
+        if batch_size != len(training_data):
+            random.shuffle(training_data)
+        learning_rate /= batch_size
+        errors = []
+        for _ in range(epochs):
+            epoch_error = 0
+            for i, sample in enumerate(training_data):
+                if i > 0 and i % batch_size == 0:
+                    self.update(learning_rate)
+                if len(sample) != 2:
+                    raise ValueError("illegal structure in training data")
+                epoch_error += self.backward(sample[0], sample[1])
+            self.update(learning_rate)
+            errors.append(epoch_error / len(training_data))
+        return errors
+
+    def update(self, learning_rate):
+        """Update the weights and biases and clear the delta variables.
+
+        Args:
+            learning_rate (float): Determines how fast the network tries to learn.
+              The bigger this value the bigger steps the network takes towards the target.
+        """
+        for i in range(1, len(self.shape)):
+            self.weights[i] -= learning_rate * self.delta_weights[i]
+            self.biases[i] -= learning_rate * self.delta_biases[i]
+            self.delta_weights[i].fill(0)
+            self.delta_biases[i].fill(0)
 
     def save(self, path=""):
         """Save current weights and biases to the disk.
--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -4,9 +4,9 @@ from network import Network
 
 class TestNetwork(unittest.TestCase):
     def setUp(self):
-        self.shape = (3, 4, 2)
+        self.shape = (2, 4, 2)
         self.network = Network(self.shape)
-        self.zeros = [0 for _ in range(self.shape[0])]
+        self.data = [([0, 0], [1, 1]),([0, 1], [1, 0]), ([1, 0], [0, 1]), ([1, 1], [0, 0])]
 
     def test_constructor_raises_error(self):
         self.assertRaises(ValueError, Network, (0, 1))
@@ -15,14 +15,31 @@ class TestNetwork(unittest.TestCase):
 
     def test_constructor_sets_attributes_shape_right(self):
         self.assertEqual(len(self.network.values), len(self.shape))
+        self.assertEqual(len(self.network.activations), len(self.shape))
+        self.assertEqual(len(self.network.gradients), len(self.shape))
         self.assertEqual(len(self.network.weights), len(self.shape))
         self.assertEqual(len(self.network.biases), len(self.shape))
+        self.assertEqual(len(self.network.delta_weights), len(self.shape))
+        self.assertEqual(len(self.network.delta_biases), len(self.shape))
         for i in range(1, len(self.shape)):
             self.assertEqual(self.network.weights[i].shape, (self.shape[i], self.shape[i - 1]))
             self.assertEqual(self.network.biases[i].shape, (self.shape[i], 1))
+            self.assertEqual(self.network.delta_weights[i].shape, self.network.weights[i].shape)
+            self.assertEqual(self.network.delta_biases[i].shape, self.network.biases[i].shape)
 
     def test_forward_returns_right_shape(self):
-        self.assertEqual(self.network.forward(self.zeros).shape, (self.shape[-1], 1))
+        self.assertEqual(self.network.forward(self.data[0][0]).shape, (self.shape[-1], 1))
+
+    def test_train_returns_right_shape(self):
+        epochs = 10
+        self.assertEqual(len(self.network.train(self.data, epochs, batch_size=3)), epochs)
+
+    def test_train_raises_error(self):
+        data = self.data
+        self.assertRaises(ValueError, self.network.train, data, 0)
+        self.assertRaises(ValueError, self.network.train, data, 1, 0)
+        data[0] = ([0, 0], [1, 1], [0, 0])
+        self.assertRaises(ValueError, self.network.train, data, 1)
 
     def test_save_and_load(self):
         old_weights = self.network.weights
