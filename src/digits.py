c9aedea0fc97117cbbd90b389d97083b9a0736bd
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-06-06 16:57:02 +0300

    Add UI docstrings

--- src/digits.py
+++ src/digits.py
@@ -1,3 +1,10 @@
+"""Digits
+=========
+
+This module is used to test the Network module's ability to learn and classify handwritten digits in
+the mnist dataset.
+"""
+
 import struct
 import math
 import gzip
@@ -8,6 +15,17 @@ import matplotlib.pyplot as plt
 from network import Network
 
 def load_labels(path):
+    """Load the labels for the digits from a gz compressed file.
+
+    Args:
+        path (str): Path to the file.
+
+    Raises:
+        ValueError: If the magic number read from the file is not equal to the expected number.
+
+    Returns:
+        ndarray: The labels.
+    """
     with gzip.open(path, 'rb') as file:
         magic, length = struct.unpack('>II', file.read(8))
         if magic != 2049:
@@ -17,6 +35,17 @@ def load_labels(path):
     return array
 
 def load_images(path):
+    """Load the handwritten digits from a gz compressed file.
+
+    Args:
+        path (str): Path to the file.
+
+    Raises:
+        ValueError: If the magic number read from the file is not equal to the expected number.
+
+    Returns:
+        ndarray: The images.
+    """
     with gzip.open(path, 'rb') as file:
         magic, length, rows, cols = struct.unpack('>IIII', file.read(16))
         if magic != 2051:
@@ -26,6 +55,11 @@ def load_images(path):
     return array.reshape(length, rows, cols) / 255
 
 def plot_images(images):
+    """Plot images one after another.
+
+    Args:
+        images (list): Images to be plotted.
+    """
     for image in images:
         plt.imshow(image[0])
         plt.set_cmap("binary")
@@ -34,6 +68,11 @@ def plot_images(images):
         plt.show()
 
 def plot_error(error):
+    """Plot the average errors per epoch during training.
+
+    Args:
+        error (list): Error to be plotted.
+    """
     plt.plot(range(1, len(error) + 1), error)
     ticks = list(range(0, len(error) + 1, math.ceil(len(error) / 10)))[1:]
     if ticks[0] != 1:
@@ -47,6 +86,16 @@ def plot_error(error):
     plt.show()
 
 def train(nn, images, labels):
+    """Train the network to recognize handwritten digits.
+
+    Args:
+        nn (Network): Neural network to be trained.
+        images (ndarray): Images to be used in training.
+        labels (ndarray): Labels to the images.
+
+    Returns:
+        list: Average errors per epoch during training.
+    """
     print("Training started.")
     training_data = []
     for i in range(len(labels)):
@@ -62,6 +111,17 @@ def train(nn, images, labels):
     return errors
 
 def test(nn, images, labels):
+    """Test how well the network is able to recognize handwritten digits.
+
+    Args:
+        nn (Network): Neural network to be tested.
+        images (ndarray): Images to be used in testing.
+        labels (ndarray): Labels to the images.
+
+    Returns:
+        tuple: Contains two lists, the first one contains images classified incorrectly and the
+          second one contains images classifiend correctly.
+    """
     print("Testing started.")
     right = []
     wrong = []
@@ -82,6 +142,12 @@ def test(nn, images, labels):
     return wrong, right
 
 def main():
+    """The main function.
+
+    Create a new neural network, load the mnist handwritten digits, test how well the network
+    performs before training, train the network, test how well the network performs after training,
+    and plot the images that the network was not able to classify correctly.
+    """
     nn = Network((784, 28, 28, 10))
 
     path = "../mnist/"
